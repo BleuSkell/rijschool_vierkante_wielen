93f313affe784cdd5dd5268a44c493bfba1b3af5
BleuSkell <kyasowi1@gmail.com>
2025-05-21 10:07:20 +0200

    overview payments desktop styling finished

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,11 +1,43 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-row justify-center">
-            <div class="bg-white overflow-hidden shadow-sm rounded-lg p-4 w-96 lg:w-full">
+            <div class="bg-gray-200 overflow-hidden shadow-sm rounded-lg px-5 py-2 w-96 lg:w-full border border-solid border-4 border-[#B9A359]">
                 @if ($payments)
                     @foreach ($payments as $payment)
-                        <div>
-                            <h1>Betaling voor factuur:</h1>
+                        <div class="flex flex-col bg-white p-2 rounded-lg shadow-md mt-4 mb-4">
+                            <div class="flex flex-row justify-between mb-2">
+                                <div>
+                                    <h1>Betaling voor factuur: {{ $payment->invoiceNumber }}</h1>
+                                    <p>Status: {{ $payment->status }}</p>
+                                </div>
+                                
+                                <div>
+                                    <p>Datum: {{ $payment->date }}</p>
+                                </div>
+                            </div>
+
+                            <div class="flex flex-row justify-between">
+                                <a href="" class="
+                                    bg-blue-500 text-white font-bold py-2 px-4 rounded
+                                    hover:bg-blue-700 transition duration-150 ease-in-out    
+                                ">
+                                    Bewerken
+                                </a>
+
+                                <form action="" method="POST" onsubmit="return confirm('Weet je zeker dat je deze factuur wilt verwijderen?');">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button 
+                                        type="submit" 
+                                        class="
+                                            bg-red-600 text-white font-bold py-2 px-4 rounded
+                                            hover:bg-red-700 transition duration-150 ease-in-out
+                                        ">
+                                        Verwijderen
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     @endforeach
                 @else   
